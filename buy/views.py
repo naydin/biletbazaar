9dd5c9a600e4b261801245a8d911c1e6b0cc9849
naydin <aydinnecati@gmail.com>
2014-02-01 17:47:02 +0200

    Bug fix in biletal1 and biletal4 is linked with dynamic data but incomplete.

--- buy/views.py
+++ buy/views.py
@@ -13,16 +13,18 @@ def biletal1(request):
     if request.method == 'POST':
         try:
             ticket_id = request.session['buy_ticket_id']
-            ticket = Ticket.objects.get(id = ticket_id)
-            if ticket_count > ticket.ticket_count:
+            ticket = Ticket.objects.get(id = ticket_id)            
+            ticket_count = request.POST['buy_ticket_count']
+            
+            if int(ticket_count) > ticket.ticketCount:
                 return redirect('/anasayfa')
             
-            ticket_count = request.POST['buy_ticket_count']
             request.session['buy_ticket_count'] = ticket_count
             
             return redirect('/biletal2')
             
         except Exception as e:
+            print '%s (%s)' % (e.message, type(e))
             return redirect('/anasayfa')
             
 
@@ -40,7 +42,7 @@ def biletal1(request):
             })
 
         except Exception as e:
-            # print '%s (%s)' % (e.message, type(e))
+            print '%s (%s)' % (e.message, type(e))
             return redirect('/anasayfa')    
     
     
@@ -86,7 +88,7 @@ def biletal2(request):
                 return redirect('/biletal3')
             
         except Exception as e:
-            # print '%s (%s)' % (e.message, type(e))
+            print '%s (%s)' % (e.message, type(e))
             return redirect('/anasayfa')
 
         
@@ -181,4 +183,16 @@ def biletal3(request):
     })
     
 def biletal4(request):
-    return render(request,'buy/biletal4.html')
+    try:
+        ticket_id = request.session['buy_ticket_id']
+        ticket_count = request.session['buy_ticket_count']
+        ticket = Ticket.objects.get(id=ticket_id)
+    except Exception as e:
+        return redirect('/anasayfa')
+        
+    return render(request,'buy/biletal4.html',{
+        'ticket':ticket,
+        'ticket_count':ticket_count
+    })
+
+
