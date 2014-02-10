846afd2c71fa1f1e55f80ba2902f1f2fa467ab3d
naydin <aydinnecati@gmail.com>
2014-02-11 01:23:25 +0200

    no message

--- biletbazaar/views.py
+++ biletbazaar/views.py
@@ -67,7 +67,7 @@ def fb_login(request):
             html = response.read()
             dict = json.loads(html)
             emaill = dict['email']
-            email= udata.encode("ascii","ignore")
+            email= emaill.encode("ascii","ignore")
             try:
                 user = User.objects.get(username=email)
                 #login if the user exists
