a9f8c2e667910e953df1e84c3b96ed549027a2de
naydin <aydinnecati@gmail.com>
2014-02-17 22:23:34 +0200

    login required import.

--- buy/views.py
+++ buy/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.template import loader
 from data.models import *
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.decorators import login_required
 from biletbazaar.validation_util import *
 
 import datetime
